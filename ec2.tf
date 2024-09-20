91e689f2fb7da612e7d9e7eeee9cbaff5360b8fc
sathishbellala36 <sathishbellala36@gmail.com>
2024-09-20 15:15:03 +0530

    terraform

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+
+
+ resource "aws_instance" "terraform" {
+   
+      ami = var.ami_id
+       instance_type = var.instance_type
+        vpc_security_group_ids = var.vpc_security_group_ids
+
+       }
+ 
